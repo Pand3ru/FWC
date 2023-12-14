c8632a3b83c67f2d497ff68921169b58a453d62e
konsti <konsti>
2023-12-14 21:24:04 +0100

    for gpt

--- functions_calculation.go
+++ functions_calculation.go
@@ -16,10 +16,7 @@ func CalculatePostfix(s *Stack) (float64, error) {
 		case 1: // Assuming 1 represents operand
 			Push(&OutputStack, slice[i])
 		case 0: // Assuming 0 represents operator
-			Operator, err := Pop(&OutputStack)
-			if err != nil {
-				return 0, err
-			}
+			Operator := slice[i].(Token).Value
 			Operand2, err := Pop(&OutputStack)
 			if err != nil {
 				return 0, err
@@ -28,10 +25,11 @@ func CalculatePostfix(s *Stack) (float64, error) {
 			if err != nil {
 				return 0, err
 			}
-			val, err := Calculate(Operator.(Token), Operand1.(Token), Operand2.(Token))
+			val, err := Calculate(Operator, Operand1.(Token), Operand2.(Token))
 			if err != nil {
 				return 0, err
 			}
+			fmt.Printf("\n=====\nTokentype: %v, TokenValue: %v, Operator: %v, Operand1: %v, Operand2: %v, OutputValue: %v \n=====\n", TokenType, slice[i].(Token).Value, Operator, Operand1, Operand2, val)
 			Push(&OutputStack, Token{Value: fmt.Sprintf("%f", val)})
 		}
 	}
@@ -43,8 +41,8 @@ func CalculatePostfix(s *Stack) (float64, error) {
 	return strconv.ParseFloat(result.(Token).Value, 64)
 }
 
-func Calculate(a Token, b Token, c Token) (float64, error) {
-	op := a.Value
+func Calculate(a string, b Token, c Token) (float64, error) {
+	op := a
 	A, err := strconv.ParseFloat(b.Value, 64)
 	if err != nil {
 		return 0, err
